51544ce4d0fb8a16481419101c78d494f516e01f
Nikita Kartashov <snailandmail@gmail.com>
2017-04-17 00:44:05 +0100

    Put path first, cleanup path

--- path.sh
+++ path.sh
@@ -7,11 +7,6 @@ function includeDirInPath {
 
 if [ -z "$WORK_PC" ]
 then
-  export CUDA_HOME=/usr/local/cuda
-  export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$CUDA_HOME/lib"
-
-  includeDirInPath "$CUDA_HOME/bin"
-  includeDirInPath "/Library/Haskell/bin"
   includeDirInPath "$HOME/bin"
   includeDirInPath "/usr/local/Homebrew/bin"
   includeDirInPath "/usr/local/bin"

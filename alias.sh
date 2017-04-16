c65ec461d305cc7b00d501cb590612e6ce1fe18a
Nikita Kartashov <nkartashov@Nikitas-MacBook-Pro.local>
2017-04-16 21:45:48 +0100

    Add function for moving from https to git github urls

--- alias.sh
+++ alias.sh
@@ -70,3 +70,12 @@ function ssh_fingeprint {
   ssh-keygen -lf "$@"
 }
 
+function git_to_ssh {
+  local CURRENT_URL='https://github.com/nkartashov/pdf-saver.git' #$(git remote -v | head -1 | cut -f2 | cut -d' ' -f1)
+  local HTTP_PREFIX='https://github.com/'
+  local SSH_PREFIX='git@github.com:'
+  local REPO=${CURRENT_URL#$HTTP_PREFIX}
+  local FULL_GIT_URL="$SSH_PREFIX$REPO"
+  git remote set-url origin $FULL_GIT_URL
+}
+

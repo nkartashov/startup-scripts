83e1402d7a529358ff04893827711de460d49e19
Nikita Kartashov <nikita.kartashov@du.co>
2017-10-26 14:13:11 +0100

    Add a way to distinguish environments

--- duco.sh
+++ duco.sh
@@ -1,15 +1,17 @@
-alias rj='mvn clean install -DskipTests && rake workers:install'
-alias db='mysql --user=root --password=password --database=tenant1'
-alias tdb='mysql --user=root --password=password --database=cube_test'
-function f {
-  rake apartment:drop &&
-  rake apartment:create &&
-  rake db:migrate &&
-  rake db:seed &&
-  rake db:trdb
-}
-alias w='cd ~/code/cube && rake ramdisk && f'
-function atlas_bxr() {
-  TEST_EN=mt-atlas bxr "$@"
-}
-alias cu='cd ~/code/cube'
+if [ "$BASH_SCRIPTS_ENV" == "DUCO" ]; then
+  alias rj='mvn clean install -DskipTests && rake workers:install'
+  alias db='mysql --user=root --password=password --database=tenant1'
+  alias tdb='mysql --user=root --password=password --database=cube_test'
+  function f {
+    rake apartment:drop &&
+    rake apartment:create &&
+    rake db:migrate &&
+    rake db:seed &&
+    rake db:trdb
+  }
+  alias w='cd ~/code/cube && rake ramdisk && f'
+  function atlas_bxr() {
+    TEST_EN=mt-atlas bxr "$@"
+  }
+  alias cu='cd ~/code/cube'
+fi

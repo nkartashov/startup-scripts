3f402431638a3dcbe6c3de1e98214679692ed6d8
Nikita Kartashov <nikita.kartashov@du.co>
2017-11-01 10:05:49 +0000

    Add a shortcut for starting Percona

--- duco.sh
+++ duco.sh
@@ -18,8 +18,20 @@ if [ "$BASH_SCRIPTS_ENV" == "DUCO" ]; then
   function cu() {
     cd ~/code/cube
   }
-  function perc() {
+  function perc_paths() {
     cu &&
     find . -name database.yml -or -name reference.conf -or -name application.conf | xargs sed -i 's/host = localhost/host = "127.0.0.1"/; s/host: localhost/host: 127.0.0.1/'
+  }
+
+  function perc_run() {
+    docker run --rm --name percona56 --tmpfs "/var/lib/mysql:rw,size=1024M" -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -d percona:5.6
+  }
+
+  function bloody_percona() {
+    sudo service landscape-client stop
+    sudo service mysql stop
+
+    # Start the container: This will fail if it's already running
+    docker run --rm --name percona56 --tmpfs "/var/lib/mysql:rw,size=1024M" -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -d percona:5.6
   }
 fi

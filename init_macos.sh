83e8dc46a750177c5eb5d685170bd48bbe141398
Nikita Kartashov <snailandmail@gmail.com>
2018-06-09 13:50:34 +0100

    Add fzf

--- init_macos.sh
+++ init_macos.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 brew cask install xquartz
-brew install haskell-stack openvpn gcc coreutils cmake fdupes ag tldr docker-machine docker-compose lua luajit imagemagick gibo
+HOME_STUFF="haskell-stack fdupes imagemagick"
+brew install $HOME_STUFF openvpn gcc coreutils cmake ag tldr docker-machine docker-compose lua luajit gibo fzf
 brew install vim --with-client-server --with-lua --with-luajit --with-tcl --with-python3
 ESSENTIALS="xquartz alfred day-o google-drive keepassxc anaconda iterm2 the-unarchiver java virtualbox"
 MESSENGERS="skype telegram whatsapp slack"
 MISC="tunnelbear electrum mplayerx tunnelblick beardedspice google-chrome openoffice steam torbrowser"
-brew cask install "$ESSENTIALS $MESSENGERS $MISC"
+brew cask install $ESSENTIALS $MESSENGERS $MISC
 curl https://raw.githubusercontent.com/nkartashov/startup-scripts/master/install.sh | sh
 curl https://raw.githubusercontent.com/nkartashov/vim-scripts/master/install.sh | sh

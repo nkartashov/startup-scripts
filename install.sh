5c0438e813e86ff78d7011f229671bd8264459c3
Nikita Kartashov <snailandmail@gmail.com>
2018-08-06 23:00:05 +0100

    Make install.sh permissive

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 RESULT_FOLDER=~/startup-scripts
-git clone git@github.com:nkartashov/startup-scripts.git $RESULT_FOLDER &&
+set -e
+if [[ ! -d $RESULT_FOLDER ]]; then
+  git clone git@github.com:nkartashov/startup-scripts.git $RESULT_FOLDER
+fi
 ln -s $RESULT_FOLDER/bashrc ~/.bashrc

f2d1c8d094411b5231d0a0f9422899dac5c47271
Nikita Kartashov <snailandmail@gmail.com>
2018-08-06 23:04:48 +0100

    Automatically download solarized

--- install.sh
+++ install.sh
@@ -6,3 +6,12 @@ if [[ ! -d $RESULT_FOLDER ]]; then
   git clone git@github.com:nkartashov/startup-scripts.git $RESULT_FOLDER
 fi
 ln -s $RESULT_FOLDER/bashrc ~/.bashrc
+
+SOLARIZED_DIR=~/solarized
+if [[ ! -d $SOLARIZED_DIR ]]; then
+  git clone git@github.com:altercation/solarized.git $SOLARIZED_DIR
+fi
+
+# TODO: add all the brew setup business
+# TODO: add brew cask font tap
+# TODO: add fira code

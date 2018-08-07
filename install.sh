63e3376db0b2876ee0ffb5a56d566152d3056f50
Nikita Kartashov <nikita.kartashov@babylonhealth.com>
2018-08-07 11:47:15 +0100

    Add a global gitignore and link it in install

--- install.sh
+++ install.sh
@@ -6,11 +6,13 @@ if [[ ! -d $RESULT_FOLDER ]]; then
   git clone git@github.com:nkartashov/startup-scripts.git $RESULT_FOLDER
 fi
 ln -s $RESULT_FOLDER/bashrc ~/.bashrc
+ln -s $RESULT_FOLDER/gitignore ~/.gitignore
 
 SOLARIZED_DIR=~/solarized
 if [[ ! -d $SOLARIZED_DIR ]]; then
   git clone git@github.com:altercation/solarized.git $SOLARIZED_DIR
 fi
+
 
 # TODO: add all the brew setup business
 # TODO: add brew cask font tap

98bbaa0d23f116122d78825e650b89ea1e17bb37
Nikita Kartashov <nikita.kartashov@babylonhealth.com>
2018-08-13 14:04:06 +0100

    Support defining local env variables that are injected in session

--- env.sh
+++ env.sh
@@ -6,4 +6,8 @@ if [ -e "$ENV_FILE" ]; then
 else
   export BASH_SCRIPTS_ENV="HOME"
 fi
+ENV_VARIABLE_FILE=~/.env_variables
+if [ -e "$ENV_VARIABLE_FILE" ]; then
+  export $(cat "$ENV_VARIABLE_FILE" | xargs)
+fi
 export EDITOR="vim"

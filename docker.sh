f0cc6bacf944a4c9d5a1544c766de0f71c6ff2e5
Nikita Kartashov <nikita.kartashov@babylonhealth.com>
2018-09-05 15:03:09 +0100

    Add docker shortcuts for ps and exec

--- docker.sh
+++ docker.sh
@@ -1,3 +1,20 @@
+function lookup_docker_id {
+  echo $(docker ps | tail +2 | head -"$1" | tail -1 | cut -f1 -d' ')
+}
 function dkillall {
   docker ps -q | xargs docker kill
+}
+function dps {
+  docker ps | nl -v 0
+}
+function d {
+  shift
+  echo docker "$@" "$CONTAINER_ID"
+  docker "$@" "$CONTAINER_ID"
+}
+function dexec {
+  CONTAINER_ID=$(lookup_docker_id "$1")
+  shift
+  echo docker exec -it "$CONTAINER_ID" "$@"
+  docker exec -it "$CONTAINER_ID" "$@"
 }

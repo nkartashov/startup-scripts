45199efcca10b2c766f9588542c48642e53c830c
Nikita Kartashov <snailandmail@gmail.com>
2018-09-11 23:01:59 +0100

    Remove anaconda

--- path.sh
+++ path.sh
@@ -11,8 +11,6 @@ then
   includeDirInPath "/usr/local/Homebrew/bin"
   includeDirInPath "/usr/local/bin"
   includeDirInPath "$HOME/.local/bin"
-  includeDirInPath "$HOME/anaconda3/bin"
-  includeDirInPath "/usr/local/anaconda3/bin"
   includeDirInPath "/usr/local/opt/icu4c/bin"
   includeDirInPath "/usr/local/opt/icu4c/sbin"
   includeDirInPath "/usr/local/opt/fzf/bin"

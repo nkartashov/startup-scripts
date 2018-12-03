ff2a205e00e05d9276f07024da6702773352d268
Nikita Kartashov <snailandmail@gmail.com>
2018-12-03 22:18:33 +0000

    Add pyenv

--- env.sh
+++ env.sh
@@ -14,3 +14,8 @@ if [ -e "$ENV_VARIABLE_FILE" ]; then
   export_env "$ENV_VARIABLE_FILE"
 fi
 export EDITOR="vim"
+
+export WORKON_HOME=~/envs
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='-p python3'
+source /usr/local/bin/virtualenvwrapper.sh

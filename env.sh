6a104b9f846b465444965b40060e1f51bd6baa01
Nick Kartashov <snailandmail@gmail.com>
2022-05-21 18:50:25 +0100

    Fix vrtualenvwrapper problems stemming from brew changing its home

--- env.sh
+++ env.sh
@@ -22,4 +22,4 @@ export PYENV_ROOT=~/.pyenv
 export WORKON_HOME=~/envs
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='-p python3'
-source /usr/local/bin/virtualenvwrapper.sh
+source virtualenvwrapper.sh

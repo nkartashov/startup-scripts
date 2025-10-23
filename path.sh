4ed974626c49dbc71a724901ac4f4fb540f23cb4
Nick Kartashov <nickkartashov@openai.com>
2025-10-23 10:31:17 +0100

    Simplify path

--- path.sh
+++ path.sh
@@ -5,15 +5,12 @@ function includeDirInPath {
   fi
 }
 
-if [ -z "$WORK_PC" ]
-then
-  includeDirInPath "$HOME/bin"
-  includeDirInPath "$HOME/.local/bin"
-  includeDirInPath "/usr/local/Homebrew/bin"
-  includeDirInPath "/opt/homebrew/bin"
-  includeDirInPath "/usr/local/bin"
-  includeDirInPath "$HOME/.local/bin"
-  includeDirInPath "/usr/local/opt/icu4c/bin"
-  includeDirInPath "/usr/local/opt/icu4c/sbin"
-  includeDirInPath "/usr/local/opt/fzf/bin"
-fi
+includeDirInPath "$HOME/bin"
+includeDirInPath "$HOME/.local/bin"
+includeDirInPath "/usr/local/Homebrew/bin"
+includeDirInPath "/usr/local/bin"
+includeDirInPath "/opt/homebrew/bin"
+includeDirInPath "$HOME/.local/bin"
+includeDirInPath "/usr/local/opt/icu4c/bin"
+includeDirInPath "/usr/local/opt/icu4c/sbin"
+includeDirInPath "/usr/local/opt/fzf/bin"

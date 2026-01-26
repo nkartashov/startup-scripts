2722f44d2793fc5aef87efb2cd93e7bd9cefd1f6
Nick Kartashov <snailandmail@gmail.com>
2026-01-26 11:39:35 +0000

    Add pyenv init steps

--- python.sh
+++ python.sh
@@ -72,3 +72,7 @@ function pytest_run_modified() {
     pytest "${files_to_test[@]}"
   fi
 }
+
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
